8cf6e2a62efc55e0fba87a98d69f12528ac3faad
bmauncho <84400423+bmauncho@users.noreply.github.com>
2023-01-08 18:53:46 +0300

    Hoop

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    private float gravityValue = -9.81f;
+    [SerializeField] float rotationSmoothTime; 
+    float currentAngle; 
+    float currentAngleVelocity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (move.magnitude >= 0.1f) 
+        {     
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+            controller.Move(move * Time.deltaTime * playerSpeed);
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity* Time.deltaTime);
+    }
+}
